e36dc30706c8ff43bf8b268575b4eb31c55eacea
noahware <noahswtf@icloud.com>
2026-09-07 11:05:04 +0100

    Add log to main

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,12 @@
-int main() {
+#include "../src/emu.hpp"
+#include <format>
+#include <cstdio>
+
+#define LOG(...) std::printf("%s\n", std::format(__VA_ARGS__).c_str())
+
+int main()
+{
+    LOG("emu");
+
     return 0;
 }
