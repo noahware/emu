5940313b8a2f619b4346d7d7b13e681c976a95ca
noahware <noahswtf@icloud.com>
2026-09-07 10:54:46 +0100

    Added map_mem

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,5 +1,41 @@
 #include "cpu.hpp"
 
+emu::status emu::cpu::map_mem(const addr_t addr, const std::size_t size)
+{
+	if (find_rgn(addr))
+	{
+		return { };
+	}
+
+	// todo: handle case where there is a region before AND after it
+	if (const auto prev_rgn = find_rgn(addr - 1))
+	{
+		prev_rgn->data.resize(prev_rgn->size() + size);
+
+		return { };
+	}
+
+	if (const auto next_rgn = find_rgn(addr + size))
+	{
+		const addr_t old_addr = next_rgn->addr;
+
+		next_rgn->addr = addr;
+		next_rgn->data.insert(next_rgn->data.begin(), size, 0);
+
+		auto node = mem_.extract(old_addr);
+		node.key() = addr;
+		mem_.insert(std::move(node));
+
+		return { };
+	}
+
+	std::unique_lock lock(mem_mutex_);
+
+	mem_[addr] = mem_region{ addr, { } };
+
+	return { };
+}
+
 emu::status emu::cpu::read_mem(const addr_t addr, const std::span<std::uint8_t> buf) const
 {
 	const auto rgn = find_rgn(addr);
