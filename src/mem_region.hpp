1113dd76b560fc7620fc39203a3bb23bbad1f070
noahware <noahswtf@icloud.com>
2026-09-07 10:12:05 +0100

    Added contains() and end_addr() to mem_region

--- src/mem_region.hpp
+++ src/mem_region.hpp
@@ -9,6 +9,16 @@ namespace emu
 		addr_t addr;
 		std::vector<std::uint8_t> data;
 
+		[[nodiscard]] bool contains(const addr_t check_addr) const noexcept
+		{
+			return addr <= check_addr && check_addr <= end_addr();
+		}
+
+		[[nodiscard]] addr_t end_addr() const noexcept
+		{
+			return addr + size();
+		}
+
 		[[nodiscard]] std::size_t size() const noexcept
 		{
 			return data.size();
