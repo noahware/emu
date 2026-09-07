1fbfd6bd019f7b9ab320a4caa89168509a12ea12
noahware <noahswtf@icloud.com>
2026-09-07 15:02:09 +0100

    Added MOV insn

--- src/arm64.cpp
+++ src/arm64.cpp
@@ -2,9 +2,10 @@
 
 emu::status emu::arm64_core::execute_insn(cpu& proc, const cs_insn& insn)
 {
+	const auto& ops = insn.detail->arm64.operands;
+
 	if (insn.id == ARM64_INS_LDR)
 	{
-		const auto& ops = insn.detail->arm64.operands;
 		const addr_t addr = state_.mem_op_addr(ops[1].mem);
 
 		const arm64_reg reg = ops[0].reg;
@@ -28,6 +29,17 @@ emu::status emu::arm64_core::execute_insn(cpu& proc, const cs_insn& insn)
 
 		std::memcpy(&val, data, size);
 		state_.set_reg(reg, val);
+	}
+	else if (insn.id == ARM64_INS_MOV)
+	{
+		const arm64_reg dest = ops[0].reg;
+
+		const arm64_reg src_reg = ops[1].reg;
+		const auto src_imm = ops[1].imm;
+		
+		const std::uint64_t src_val = ops[1].type == ARM64_OP_REG ? state_.reg(src_reg) : src_imm;
+
+		state_.set_reg(dest, src_val);
 	}
 	else
 	{
