10f3a68a5db18e165a99b7bd03a58be1ee388098
noahware <noahswtf@icloud.com>
2026-09-07 10:40:03 +0100

    Added mem regions management to cpu

--- src/cpu.hpp
+++ src/cpu.hpp
@@ -1,9 +1,10 @@
 #pragma once
-#include <cstdint>
-#include <cstddef>
-#include <span>
-
 #include "defs.hpp"
+#include <span>
+#include <map>
+#include <shared_mutex>
+
+#include "mem_region.hpp"
 
 namespace emu
 {
@@ -19,5 +20,43 @@ namespace emu
 		virtual status map_mem(addr_t addr, std::size_t size) = 0;
 		virtual status read_mem(addr_t addr, std::span<std::uint8_t> buf) = 0;
 		virtual status write_mem(addr_t addr, std::span<const std::uint8_t> buf) = 0;
+
+	protected:
+		[[nodiscard]] rgn_ref_const find_rgn(const addr_t addr) const noexcept
+		{
+			std::shared_lock lock(mem_mutex_);
+
+			auto it = mem_.upper_bound(addr);
+			if (it != mem_.begin())
+			{
+				--it;
+				if (it->second.contains(addr))
+				{
+					return rgn_ref_const{ &it->second, std::move(lock) };
+				}
+			}
+
+			return { };
+		}
+
+		[[nodiscard]] rgn_ref_mut find_rgn(const addr_t addr) noexcept
+		{
+			std::unique_lock lock(mem_mutex_);
+
+			auto it = mem_.upper_bound(addr);
+			if (it != mem_.begin())
+			{
+				--it;
+				if (it->second.contains(addr))
+				{
+					return rgn_ref_mut{ &it->second, std::move(lock) };
+				}
+			}
+
+			return { };
+		}
+
+		mutable std::shared_mutex mem_mutex_;
+		std::map<addr_t, mem_region> mem_;
 	};
 }
