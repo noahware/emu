6587a2658a1e457d0af576b21d20a8bf08605b4a
noahware <noahswtf@icloud.com>
2026-09-07 10:48:14 +0100

    Added write_mem impl

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -21,3 +21,25 @@ emu::status emu::cpu::read_mem(const addr_t addr, const std::span<std::uint8_t> 
 
 	return { };
 }
+
+emu::status emu::cpu::write_mem(const addr_t addr, const std::span<const std::uint8_t> buf)
+{
+	const auto rgn = find_rgn(addr);
+
+	if (!rgn)
+	{
+		return { };
+	}
+
+	const auto off = rgn->offset_of(addr);
+	const auto end_off = off + buf.size();
+
+	if (rgn->size() < end_off)
+	{
+		return { };
+	}
+
+	std::memcpy(rgn->data.data() + off, buf.data(), buf.size());
+
+	return { };
+}
